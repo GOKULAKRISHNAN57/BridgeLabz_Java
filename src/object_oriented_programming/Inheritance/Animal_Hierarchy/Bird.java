0515a262607ca2e1c43a2e9e387fa1713dc20c74
M. Gokulakrishnan <149427308+GOKULAKRISHNAN57@users.noreply.github.com>
2026-05-06 10:30:31 +0530

    Delete src/object_oriented_programming directory

--- src/object_oriented_programming/Inheritance/Animal_Hierarchy/Bird.java
+++
@@ -1,9 +0,0 @@
-package object_oriented_programming.Inheritance.Animal_Hierarchy;
-
-public class Bird extends Animal{
-
-    @Override
-    public void makeSound(){
-        System.out.println("Bird goes kaka");
-    }
-}
--- src/object_oriented_programming/Inheritance/Animal_Hierarchy/Dog.java
+++
@@ -1,9 +0,0 @@
-package object_oriented_programming.Inheritance.Animal_Hierarchy;
-
-public class Dog extends Animal{
-
-    @Override
-    public void makeSound(){
-        System.out.println("Dog goes Woff");
-    }
-}
