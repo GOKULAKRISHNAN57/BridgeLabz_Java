7f52ce364186fe10be81f5d82567dfdffc32aab8
M. Gokulakrishnan <149427308+GOKULAKRISHNAN57@users.noreply.github.com>
2026-06-02 12:16:06 +0530

    Delete src/object_oriented_programming directory

--- src/object_oriented_programming/Inheritance/Animal_Hierarchy/Cat.java
+++
@@ -1,9 +0,0 @@
-package object_oriented_programming.Inheritance.Animal_Hierarchy;
-
-public class Cat extends Animal{
-
-    @Override
-    public void makeSound(){
-        System.out.println("Cat goes Meow");
-    }
-}
